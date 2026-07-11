f48f0ea29b7171bf5745b1a6c76b49db7dc7339a
gdslwGIT <gdeslawa@gmail.com>
2026-07-11 16:36:10 +0300

    refactor: fix typo in forgot password page

--- pages/forgot_password_page.py
+++ pages/forgot_password_page.py
@@ -4,9 +4,9 @@ class ForgotPasswordPage:
     def __init__(self, page: Page):
         self.page = page
         self.email_form = page.locator("#email")
-        self.retrive_password_button = page.locator("button[type='submit']")
+        self.retrieve_password_button = page.locator("button[type='submit']")
 
     def retrieve_password(self, email):
         self.email_form.fill(email)
-        self.retrive_password_button.click()
+        self.retrieve_password_button.click()
 
